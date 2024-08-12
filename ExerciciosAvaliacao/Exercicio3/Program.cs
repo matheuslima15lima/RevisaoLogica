af276a829c99013ee184314eb5250f07b58d74f3
Matheus Lima <limalmatheus7@gmail.com>
2024-08-12 16:35:57 -0300

    organizando exercicios

---
+++ ExerciciosAvaliacao/Exercicio3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// for (int i = 0; i <=100; i++)
+// {
+//     while (true)
+//     {
+        
+//     }
+// }
