2901095eb97537edb0c415e07f86030b493f9e82
Matheus Lima <limalmatheus7@gmail.com>
2024-08-12 17:21:30 -0300

    organizando exercicios

--- ExerciciosAvaliacao/Exercicio3/Program.cs
+++ ExerciciosAvaliacao/Exercicio3/Program.cs
@@ -1,10 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] numeros = [1,2,3,4,5,6,7,8,9,10];
+int soma = 0;
+for (int i = 0; i <=9; i++)
+{
+    if (numeros[i]%2 == 0){
+            soma+=numeros[i];
+    }
+  
+}
 
-// for (int i = 0; i <=100; i++)
-// {
-//     while (true)
-//     {
-        
-//     }
-// }
+Console.WriteLine($"{soma}");
--- ExerciciosAvaliacao/Exercicio4/obj/Debug/net8.0/Exercicio4.AssemblyInfo.cs
+++ ExerciciosAvaliacao/Exercicio4/obj/Debug/net8.0/Exercicio4.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57fb46d784336134af4c42eb053d253e151d7c82")]
-[assembly: System.Reflection.AssemblyProductAttribute("Exercicio3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Exercicio3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exercicio4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exercicio4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
